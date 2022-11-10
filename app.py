70bcea86a40a5d3b2255f23c18059f90a87f48c0
mzhafranh <16520139@std.stei.itb.ac.id>
2022-11-10 21:38:08 +0700

    Tutorial Done

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    TODO: Render the home page provided under templates/index.html in the repository
-    """
-    return "TODO"
+    return render_template("index.html")
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    args = request.args.get("q")
+    return redirect(f"https://google.com/search?q={args}")
+
+@app.get("/utub")
+def utubSearch():
+    args = request.args.get("qy")
+    return redirect(f"https://www.youtube.com/results?search_query={args}")
 
 if __name__ == "__main__":
     app.run()
