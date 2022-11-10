110166d909d243713bc4b9af9557150b27278a0e
mzhafranh <16520139@std.stei.itb.ac.id>
2022-11-10 20:58:11 +0700

    Tutorial done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from html.entities import html5
 from pydoc import HTMLDoc
 from flask import Flask, jsonify, request
+import random
 
 # Intitialise the app
 app = Flask(__name__)
@@ -25,9 +26,8 @@ def indexGreet():
 def fancyIndex():
     return '<!doctype html><html><head><meta charset="UTF-8"><title>Sisterin STI 20 Kel 1</title></head><body><pre>            _____                    _____                    _____            _____           _______         \n           /\    \                  /\    \                  /\    \          /\    \         /::\    \        \n          /::\____\                /::\    \                /::\____\        /::\____\       /::::\    \       \n         /:::/    /               /::::\    \              /:::/    /       /:::/    /      /::::::\    \      \n        /:::/    /               /::::::\    \            /:::/    /       /:::/    /      /::::::::\    \     \n       /:::/    /               /:::/\:::\    \          /:::/    /       /:::/    /      /:::/~~\:::\    \    \n      /:::/____/               /:::/__\:::\    \        /:::/    /       /:::/    /      /:::/    \:::\    \   \n     /::::\    \              /::::\   \:::\    \      /:::/    /       /:::/    /      /:::/    / \:::\    \  \n    /::::::\    \   _____    /::::::\   \:::\    \    /:::/    /       /:::/    /      /:::/____/   \:::\____\ \n   /:::/\:::\    \ /\    \  /:::/\:::\   \:::\    \  /:::/    /       /:::/    /      |:::|    |     |:::|    |\n  /:::/  \:::\    /::\____\/:::/__\:::\   \:::\____\/:::/____/       /:::/____/       |:::|____|     |:::|    |\n  \::/    \:::\  /:::/    /\:::\   \:::\   \::/    /\:::\    \       \:::\    \        \:::\    \   /:::/    / \n   \/____/ \:::\/:::/    /  \:::\   \:::\   \/____/  \:::\    \       \:::\    \        \:::\    \ /:::/    /  \n            \::::::/    /    \:::\   \:::\    \       \:::\    \       \:::\    \        \:::\    /:::/    /   \n             \::::/    /      \:::\   \:::\____\       \:::\    \       \:::\    \        \:::\__/:::/    /    \n             /:::/    /        \:::\   \::/    /        \:::\    \       \:::\    \        \::::::::/    /     \n            /:::/    /          \:::\   \/____/          \:::\    \       \:::\    \        \::::::/    /      \n           /:::/    /            \:::\    \               \:::\    \       \:::\    \        \::::/    /       \n          /:::/    /              \:::\____\               \:::\____\       \:::\____\        \::/____/        \n          \::/    /                \::/    /                \::/    /        \::/    /         ~~              \n           \/____/                  \/____/                  \/____/          \/____/                          \n                                                                                                               </pre></body></html>'
 """
-
-#Chapter 4
 """
+#Chapter 4
 @app.get("/greet")
 def indexGreet():
     name = request.args.get("name")
@@ -49,7 +49,27 @@ def indexAge():
         else:
             response = {"data" : f"You are {age} year(s) old"}
             return jsonify(response)
+
+@app.get("/flip")
+def indexFlip():
+    side = request.args.get("side")
+    winningSide = (random.randint(0, 9))
+    if not side or (side != "head" and side != "tail") :
+        return jsonify({"status" : "error", "message" : "No valid coin side was given"})    
+    elif winningSide % 2 == 0 and side == "tail":
+        response = {"data" : f"You flipped tail!, You win!"}
+        return jsonify(response)
+    elif winningSide % 2 == 0 and side == "head":
+        response = {"data" : f"You flipped tail!, You lose!"}
+        return jsonify(response)
+    elif winningSide % 2 != 0 and side == "head":
+        response = {"data" : f"You flipped head!, You win!"}
+        return jsonify(response)
+    else:
+        response = {"data" : f"You flipped head!, You lose!"}
+        return jsonify(response)
 """
+
 #Chapter 5
 @app.get("/greet")
 def indexGreet():
@@ -62,7 +82,7 @@ def indexGreet():
     else:
         return jsonify({"data" : f"Hello, {fname} {lname}!"})
 
-@app.get("/square")
+@app.get("/rectangle")
 def indexDraw():
     width = int(request.args.get("width"))
     height = int(request.args.get("height"))
@@ -75,6 +95,8 @@ def indexDraw():
                 response += "[#]"
             response += "\n"
         return f"<pre>{response}"
+
+
 
 
 
